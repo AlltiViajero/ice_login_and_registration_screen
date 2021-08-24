fbce7b9a508bb912c0472e6411022538e680dbe8
Wojciech Siudy <wojciechsiudy96@gmail.com>
2021-08-25 01:05:25 +0200

    stylization buttons, inputs, placeholder

--- js.js
+++ js.js
@@ -9,11 +9,13 @@ const reg = document.getElementById('reg');
 lo.addEventListener("click", function log_in(){
     register.style.left = "400px";
     login.style.left = "0px";
-    btn.style.left = "0px"
+    btn.style.left = "0px";
+    btn.style.background = " linear-gradient(to left, rgb(228, 216, 216),rgb(3, 187, 178), rgb(41, 144, 195))";
 }, false);
 
 reg.addEventListener("click", function reg_ister(){
     register.style.left = "0px";
     login.style.left = "-400px";
-    btn.style.left = "110px"
+    btn.style.left = "130px";
+    btn.style.background = " linear-gradient(to right, rgb(228, 216, 216),rgb(3, 187, 178), rgb(41, 144, 195))";
 }, false);
